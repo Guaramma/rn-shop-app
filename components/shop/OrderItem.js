2077f9bf10f5ca8b9bc62cb3d6a46d9eaf54ac36
Guaramma <61195555+Guaramma@users.noreply.github.com>
2020-06-13 18:57:41 +0300

    Push project

---
+++ components/shop/OrderItem.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+import Colors from "../../constants/Colors";
+import CartItem from "../../components/shop/CartItem";
+import Card from "../UI/Card";
+
+const OrderItem = (props) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  return (
+    <Card style={styles.orderItem}>
+      <View style={styles.summary}>
+        <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
+        <Text style={styles.date}>{props.date}</Text>
+      </View>
+      <Button
+        title={showDetails ? "Hide Details" : "Show Details"}
+        color={Colors.primary}
+        onPress={() => {
+          setShowDetails((prevState) => !prevState);
+        }}
+      />
+      {showDetails && (
+        <View style={styles.detailItems}>
+          {props.items.map((cartItem) => (
+            <CartItem
+              quantity={cartItem.quantity}
+              amount={cartItem.sum}
+              title={cartItem.productTitle}
+              key={cartItem.productId}
+            />
+          ))}
+        </View>
+      )}
+    </Card>
+  );
+};
+
+export default OrderItem;
+
+const styles = StyleSheet.create({
+  orderItem: {
+    backgroundColor: "white",
+    margin: 20,
+    padding: 10,
+    alignItems: "center",
+  },
+  summary: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 15,
+  },
+  totalAmount: {
+    fontFamily: "open-sans-bold",
+    fontSize: 16,
+  },
+  date: {
+    fontSize: 16,
+    fontFamily: "open-sans",
+    color: "#888",
+  },
+  detailItems: {
+    width: "100%",
+  },
+});
---
+++ store/reducers/cart.js
@@ -0,0 +1,73 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
+import CartItem from "../../models/cart-item";
+import { ADD_ORDER } from "../actions/orders";
+import { DELETE_PRODUCT } from "../actions/products";
+
+const initialState = {
+  items: {},
+  totalAmount: 0,
+};
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const prodPrice = addedProduct.price;
+      const prodTitle = addedProduct.title;
+
+      let updatedOrNewCardItem;
+      if (state.items[addedProduct.id]) {
+        updatedOrNewCardItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodTitle,
+          state.items[addedProduct.id].sum + prodPrice
+        );
+      } else {
+        updatedOrNewCardItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updatedOrNewCardItem },
+        totalAmount: state.totalAmount + prodPrice,
+      };
+
+    case REMOVE_FROM_CART:
+      const selectedCartItem = state.items[action.pid];
+      const currentQty = selectedCartItem.quantity;
+
+      let updatedCartItems;
+      if (currentQty > 1) {
+        const updatedCartItem = new CartItem(
+          selectedCartItem.quantity - 1,
+          selectedCartItem.productPrice,
+          selectedCartItem.productTitle,
+          (selectedCartItem.sum - selectedCartItem.productPrice).toFixed(2)
+        );
+        updatedCartItems = { ...state.items, [action.pid]: updatedCartItem };
+      } else {
+        updatedCartItems = { ...state.items };
+        delete updatedCartItems[action.pid];
+      }
+
+      const total =
+        state.totalAmount.toFixed(2) - selectedCartItem.productPrice.toFixed(2);
+
+    case ADD_ORDER:
+      return initialState;
+    case DELETE_PRODUCT:
+      if (!state.items[action.pid]) {
+        return state;
+      }
+      const updatedItems = { ...state.items };
+      const itemTotal = state.items[action.pid].sum;
+      delete updatedItems[action.pid];
+      return {
+        ...state,
+        items: updatedItems,
+        totalAmount: state.totalAmount.toFixed(2) - itemTotal.toFixed(2),
+      };
+    default:
+      return state;
+  }
+};
